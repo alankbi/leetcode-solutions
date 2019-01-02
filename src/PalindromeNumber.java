69eb75ca7a8a796b08d8562b12988ab19cf7099f
Alan-Bi <alan.bi326@gmail.com>
2019-01-01 22:21:19 -0800

    Problem 9: Palindrome Number

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,27 @@
+/**
+ * Problem 9: Palindrome Number
+ */
+class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        int reverse = 0;
+
+        while (temp != 0) {
+            reverse *= 10;
+            reverse += temp % 10;
+            temp /= 10;
+        }
+
+        return reverse == x;
+    }
+
+    public static void main(String[] args) {
+        boolean isPalindrome = new PalindromeNumber().isPalindrome(121);
+        System.out.println(isPalindrome);
+    }
+}
