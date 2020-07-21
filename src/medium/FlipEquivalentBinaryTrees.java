651e13d05061ad293b537b10f2538928c607231b
Alan Bi <alan.bi326@gmail.com>
2020-07-20 20:59:05 -0700

    Problem 951: Flip Equivalent Binary Trees

---
+++ src/medium/FlipEquivalentBinaryTrees.java
@@ -0,0 +1,45 @@
+package medium;
+
+import util.TreeNode;
+
+/**
+ * Problem 951: Flip Equivalent Binary Trees
+ */
+public class FlipEquivalentBinaryTrees {
+
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+
+        if (root1 == null || root2 == null || root1.val != root2.val) {
+            return false;
+        }
+
+        return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) ||
+                flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+        root1.left.left = new TreeNode(4);
+        root1.left.right = new TreeNode(5);
+        root1.right.left = new TreeNode(6);
+        root1.left.right.left = new TreeNode(7);
+        root1.left.right.right = new TreeNode(8);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(3);
+        root2.right = new TreeNode(2);
+        root2.left.right = new TreeNode(6);
+        root2.right.left = new TreeNode(4);
+        root2.right.right = new TreeNode(5);
+        root2.right.right.left = new TreeNode(8);
+        root2.right.right.right = new TreeNode(7);
+
+        boolean result = new FlipEquivalentBinaryTrees().flipEquiv(root1, root2);
+        System.out.println(result);
+    }
+}
