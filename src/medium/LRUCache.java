2b1b89e8f59e97637a27808f7b7ae2eea6091392
Alan Bi <alan.bi326@gmail.com>
2020-09-02 22:28:21 -0700

    Problem 146: LRU Cache (Improved Solution)

--- src/medium/LRUCache.java
+++ src/medium/LRUCache.java
@@ -1,68 +1,82 @@
 package medium;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
 
 /**
  * Problem 146: LRU Cache
  */
 public class LRUCache {
     private int capacity;
-    private Map<Integer, CacheItem> cache;
-    private Queue<Integer> order;
-
+    private Map<Integer, Node> map;
+    private Node first;
+    private Node last;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        cache = new HashMap<>(capacity);
-        order = new LinkedList<>();
+        map = new HashMap<>();
+        first = new Node();
+        last = new Node();
+
+        first.next = last;
+        last.prev = first;
     }
 
     public int get(int key) {
-        if (!cache.containsKey(key)) {
+        if (!map.containsKey(key)) {
             return -1;
         }
-        CacheItem item = cache.get(key);
-        item.updateCount++;
-        order.add(key);
-        return item.value;
+
+        Node node = map.get(key);
+        updateCache(node);
+        return node.val;
     }
 
     public void put(int key, int value) {
-        if (cache.containsKey(key)) {
-            CacheItem item  = cache.get(key);
-            item.value = value;
-            item.updateCount++;
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.val = value;
+            updateCache(node);
         } else {
-            cache.put(key, new CacheItem(value));
-        }
-        order.add(key);
-
-        while (cache.size() > capacity) {
-            int last = order.remove();
-            CacheItem item = cache.get(last);
-            if (item.updateCount <= 0) {
-                cache.remove(last);
-            } else {
-                item.updateCount--;
+            if (map.size() >= capacity) {
+                Node node = last.prev;
+                removeNode(node);
+                map.remove(node.key);
             }
+
+            Node node = new Node();
+            node.key = key;
+            node.val = value;
+            map.put(key, node);
+            addToFront(node);
         }
     }
 
-    private static class CacheItem {
-        private int value;
-        private int updateCount;
+    private void updateCache(Node node) {
+        removeNode(node);
+        addToFront(node);
+    }
 
-        public CacheItem(int value) {
-            this.value = value;
-        }
+    private void removeNode(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
 
-        @Override
-        public String toString() {
-            return Integer.toString(value);
-        }
+    private void addToFront(Node node) {
+        Node temp = first.next;
+        first.next = node;
+        node.prev = first;
+        node.next = temp;
+        temp.prev = node;
+    }
+
+    private static class Node {
+        public int key;
+        public int val;
+        public Node prev;
+        public Node next;
     }
 
     public static void main(String[] args) {
@@ -77,6 +91,6 @@ public class LRUCache {
         System.out.println(cache.get(3));
         System.out.println(cache.get(4));
 
-        System.out.println(cache.cache);
+        System.out.println(cache.map);
     }
 }
